fe592446bb23d20ca472f7727bbe827cfe22dbac
s22310689-uk <s22310689@student.unklab.ac.id>
2025-10-30 21:30:08 +0800

    Add files via upload

---
+++ Final project/Final project 25%/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    
+
+    private void CenterPanel()
+        {
+            panel1.Left = (this.ClientSize.Width - panel1.Width) / 2;
+            panel1.Top = (this.ClientSize.Height - panel1.Height) / 2;
+        }
+
+        
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            CenterPanel();
+        }
+
+        private void Form2_Resize(object sender, EventArgs e)
+        {
+            CenterPanel();
+        }
+    }
+}
+
